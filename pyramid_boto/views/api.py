4cb0bacb50d3f1cb679f0ec6b52569dfd269bc55
Admin <admin@MacBook-Pro-di-Admin.local>
2019-06-24 15:42:51 +0300

    Turn instance loading to AJAX

---
+++ pyramid_boto/views/api.py
@@ -0,0 +1,37 @@
+from pyramid.view import view_config
+from pyramid_boto.ec2_manager import EC2Client
+
+
+def is_active(state_code):
+    return state_code != 16
+
+
+@view_config(route_name='api_ec2', renderer='json', request_method='GET')
+def get_api_ec2_view(request):
+    """Fetch instances and show it at UI"""
+    instances = []
+    client = EC2Client()
+    client.fetch()
+    instance_dict = client.get_instances()
+    for instance in instance_dict:
+        instances.append({
+            'instance_id': instance,
+            'name': instance_dict[instance]['InstanceType'],
+            'zone': instance_dict[instance]['Placement']['AvailabilityZone'],
+            'state': instance_dict[instance]['State']['Name'],
+            'state_code': instance_dict[instance]['State']['Code'],
+            'is_active': is_active(instance_dict[instance]['State']['Code'])
+        })
+    return {'instances': instances}
+
+
+@view_config(route_name='api_ec2', renderer='json', request_method='POST')
+def post_api_ec2_view(request):
+    """Receive start/stop signal and apply it"""
+    data = request.json_body
+    selected = EC2Client().get_specified_instance(data.get('id'), data.get('zone'))
+    if is_active(data.get('state')):
+        selected.start()
+    else:
+        selected.stop()
+    return {'ID': data.get('id'), 'REGION': data.get('region')}
--- pyramid_boto/views/default.py
+++ pyramid_boto/views/default.py
@@ -1,5 +1,4 @@
 from pyramid.view import view_config
-from pyramid_boto.ec2_manager import EC2Client
 
 
 def is_active(state_code):
@@ -9,32 +8,5 @@ def is_active(state_code):
 @view_config(route_name='home', renderer='../templates/mytemplate.jinja2', request_method='GET')
 def get_home_view(request):
     """Fetch instances and show it at UI"""
-    instances = []
-    client = EC2Client()
-    client.fetch()
-    instance_dict = client.get_instances()
-    for instance in instance_dict:
-        instances.append({
-            'instance_id': instance,
-            'name': instance_dict[instance]['InstanceType'],
-            'zone': instance_dict[instance]['Placement']['AvailabilityZone'],
-            'state': instance_dict[instance]['State']['Name'],
-            'state_code': instance_dict[instance]['State']['Code'],
-            'is_active': is_active(instance_dict[instance]['State']['Code'])
-        })
-    return {'instances': instances}
+    return {}
 
-
-@view_config(request_method='POST', renderer="json", route_name="home")
-def post_home_view(request):
-    """Receive start/stop signal and apply it"""
-    data = request.json_body
-    selected = EC2Client().get_specified_instance(data.get('id'), data.get('zone'))
-    print(selected)
-    if is_active(data.get('state')):
-        selected.start()
-        print('start')
-    else:
-        selected.stop()
-        print('stop')
-    return {'ID': data.get('id'), 'REGION': data.get('region')}
