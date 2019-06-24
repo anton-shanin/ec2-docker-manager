b34275480c11a02583cd64097183149492efed19
Admin <admin@MacBook-Pro-di-Admin.local>
2019-06-24 18:50:58 +0300

    Remove misc from manager

--- pyramid_boto/ec2_manager.py
+++ pyramid_boto/ec2_manager.py
@@ -57,9 +57,3 @@ class EC2Client(metaclass=Singleton):
         """
 
         return self.instances
-
-
-if __name__ == '__main__':
-    client = EC2Client()
-    client.fetch()
-    print(client.get_instances())
